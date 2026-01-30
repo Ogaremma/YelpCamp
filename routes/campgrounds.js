bdbbc25be2e06a74a4cbe9283ae6f8f8c22d0cad
Ogaremma <emmawills725@gmail.com>
2026-01-30 03:12:49 +0100

    seeting up the review routes

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,6 +1,6 @@
-const express = require('expresss');
-const catchAsync = require('../Utils/catchAsync');
+const express = require('express');
 const router = express.Router();
+const catchAsync = require('../Utils/catchAsync');
 const ExpressError = require('../Utils/ExpressError');
 const Campground = require('../models/campground');
 const { campgroundSchema } = require('../schema');
