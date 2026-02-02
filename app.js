3a227e7481fa54cf2d1d48b2c6c8a51a7696b7c6
Ogaremma <emmawills725@gmail.com>
2026-02-02 01:12:47 +0100

    Set up session cookies

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const session = require('express-session');
 const ExpressError = require('./Utils/ExpressError');
 const methodOverride = require('method-override');
 
 
 const campgrounds = require('./routes/campgrounds');
 const reviews = require('./routes/review');
+const e = require('express');
 
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp')
@@ -27,6 +29,19 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+const sessionConfig = {
+  secret: 'thisshouldbeabettersecret',
+  resave: false,
+  saveUnintialized: true,
+  cookie: {
+    httpOnly: true,
+    expire: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7
+  }
+}
+app.use(session(sessionConfig));
 
 
 app.use('/campgrounds', campgrounds);
