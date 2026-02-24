5d082a5e0917da78f1def029101e3f53243600eb
Ogaremma <emmawills725@gmail.com>
2026-02-24 01:32:03 +0100

    setting the register methods

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const session = require('express-session');
 const flash = require('connect-flash');
 const ExpressError = require('./Utils/ExpressError');
 const methodOverride = require('method-override');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
 
 const campgrounds = require('./routes/campgrounds');
@@ -45,11 +48,24 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
   res.locals.success = req.flash('success');
   res.locals.error = req.flash('error');
   next();
 });
+
+app.get('/fakeUser', async (req, res) => {
+  const user = new User({ email: 'emmawills725@gmail.com', username: 'Ogar Emma' })
+  const newUser = await User.register(user, 'Emmasky');
+  res.send(newUser);
+})
 
 app.use('/campgrounds', campgrounds);
 app.use('/campgrounds/:id/reviews', reviews);
