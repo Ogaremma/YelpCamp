6fc08a1a87e891680eaf50db14a9d9bf40746499
Ogaremma <emmawills725@gmail.com>
2026-02-27 11:36:52 +0100

    registering user setup & errors

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const catchAsync = require('../Utils/catchAsync');
 const User = require('../models/user');
 
 router.get('/register', (req, res) => {
-    res.render('users/register')
+    res.render('users/register');
 })
-router.post('/register', async (req, res) => {
-    res.send(req.body);
-})
+
+router.post('/register', catchAsync(async (req, res) => {
+    try {
+        const { email, username, password } = req.body;
+        const user = new User({ email, username });
+        const registeredUser = await User.register(user, password);
+        console.log(registeredUser);
+        req.flash('success', 'Welcome to Yelpcamp');
+        res.redirect('/campgrounds');
+    } catch (e) {
+        req.flash('error', e.message);
+        res.redirect('register');
+    }
+}))
+
+
 module.exports = router;
