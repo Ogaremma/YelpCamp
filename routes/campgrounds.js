c897c917346892e460acfdacc4eed3a477e38bbf
Ogaremma <emmawills725@gmail.com>
2026-03-01 20:33:01 +0100

    user login verification

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -60,7 +60,7 @@ router.get('/:id/update', isLoggedIn, catchAsync(async (req, res, next) => {
 }));
 
 router.put('/:id', isLoggedIn, validateCampground, catchAsync(async (req, res, next) => {
-    const { id } = req.params;
+    const { id } = req.params;s
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     req.flash('success', 'Successfully updated campground!');
     res.redirect(`/campgrounds/${campground._id}`);
