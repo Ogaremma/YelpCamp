bb3dabdb133fb54f534fcbc6320c7ba275bb0d65
Ogaremma <emmawills725@gmail.com>
2026-03-02 12:20:35 +0100

    configuring the return to behaviour

--- middleware.js
+++ middleware.js
@@ -1,8 +1,17 @@
 module.exports.isLoggedIn = (req, res, next) => {
-    console.log("REQ.USER...", req.user);
     if (!req.isAuthenticated()) {
+        //store the url they are requesting!
+        console.log(req.path, req.originalUrl);
+        req.session.returnTo = req.originalUrl;
         req.flash('error', 'You must be signed in');
         return res.redirect('/login');
+    }
+    next();
+}
+
+module.exports.storeReturnTo = (req, res, next) => {
+    if (req.session.returnTo) {
+        res.locals.returnTo = req.session.returnTo;
     }
     next();
 }
