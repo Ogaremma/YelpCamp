a84a16e9784067965c1b334fa7fe5d4b2309524c
Ogaremma <emmawills725@gmail.com>
2026-03-05 00:58:25 +0100

    grouping the routes files codes

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -5,23 +5,21 @@ const catchAsync = require('../Utils/catchAsync');
 const Campground = require('../models/campground');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 
-
-
-
-router.get('/', catchAsync());
+router.route('/')
+    .get(catchAsync(campgrounds.index))
+    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
-router.post('/', isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+router.route('/:id')
+    .get(catchAsync(campgrounds.showCampground))
+    .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
+    .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.destroyCampground));
 
-// this is the route for the show page
-router.get('/:id', catchAsync(campgrounds.showCampground));
+
+
 
 
 router.get('/:id/update', isLoggedIn, isAuthor, catchAsync(campgrounds.renderUpdateForm));
-
-router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground));
-
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync(campgrounds.destroyCampground));
 
 module.exports = router;
