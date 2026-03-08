d5315f9ba0ad9da73a9fe2587839bd78da4d3649
Ogaremma <emmawills725@gmail.com>
2026-03-08 17:54:26 +0100

    setting up my private credentials

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,10 +4,16 @@ const campgrounds = require('../controllers/campgrounds');
 const catchAsync = require('../Utils/catchAsync');
 const Campground = require('../models/campground');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+    //  .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+    .post(upload.single('image'), (req, res) => {
+        console.log(req.body, req.file);
+        res.send('It Worked');
+    })
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
@@ -15,9 +21,6 @@ router.route('/:id')
     .get(catchAsync(campgrounds.showCampground))
     .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
     .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.destroyCampground));
-
-
-
 
 
 router.get('/:id/update', isLoggedIn, isAuthor, catchAsync(campgrounds.renderUpdateForm));
